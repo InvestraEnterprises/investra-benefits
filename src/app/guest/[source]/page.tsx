619f8c998c8d6e9ae51d7c2246aaf20f14cb734b
InvestraEnterprises <marko@investra.io>
2026-08-18 16:07:49 +0300

    Update page.tsx

--- src/app/guest/[source]/page.tsx
+++ src/app/guest/[source]/page.tsx
@@ -1,5 +1,7 @@
 import GuestView from '@/components/GuestView';
 
+export const dynamic = 'force-dynamic';
+
 export default function GuestSourcePage({ params }: { params: { source: string } }) {
   return <GuestView source={params.source} />
 }
